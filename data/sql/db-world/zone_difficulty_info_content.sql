052da403b7f942435dc21adcf6050de173ad4d98
Deawalk <zgdmemail@gmail.com>
2023-04-02 07:20:47 +0800

    Feat:修改斯坦索姆、通灵学院、黑石塔的副本强度

--- data/sql/db-world/zone_difficulty_info_content.sql
+++ data/sql/db-world/zone_difficulty_info_content.sql
@@ -1,9 +1,17 @@
 DELETE FROM `zone_difficulty_info`;
 INSERT INTO `zone_difficulty_info` (`MapId`, `PhaseMask`, `HealingNerfValue`, `AbsorbNerfValue`, `MeleeDmgBuffValue`, `SpellDmgBuffValue`, `Enabled`, `Comment`) VALUES
+
+
+-- Dungeons 
+(229, 0, '0.50', '0.50', '1.50', '1.30', 1, '黑石塔 Healing 50% / Absorb 50% Nerf / 50% physical & 30% spell damage buff'),
+(289, 0, '0.50', '0.50', '1.50', '1.30', 1, '通灵学院 Healing 50% / Absorb 50% Nerf / 50% physical & 30% spell damage buff'),
+(329, 0, '0.50', '0.50', '1.50', '1.30', 1, '斯坦索姆 Healing 50% / Absorb 50% Nerf / 50% physical & 30% spell damage buff'),
+
+
 -- Raids
 (531, 0, '0.50', '0.50', '1.50', '1.30', 1, 'AQ40 Healing 50% / Absorb 50% Nerf / 50% physical & 30% spell damage buff'),
 (509, 0, '0.50', '0.50', '1.60', '1.30', 1, 'AQ20 Healing 50% / Absorb 50% Nerf / 60% physical & 30% spell damage buff'),
-(309, 0, '0.50', '0.50', '1.50', '1.10', 1, 'ZG20 Healing 50% / Absorb 50% Nerf / 50% physical & 10% spell damage buff'),
+(309, 0, '0.50', '0.50', '1.50', '1.30', 1, 'ZG20 Healing 50% / Absorb 50% Nerf / 50% physical & 10% spell damage buff'),
 (469, 0, '0.50', '0.50', '1.50', '1.50', 1, 'BWL Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
 (249, 0, '0.50', '0.50', '1.40', '1.40', 1, 'ONY Healing 50% / Absorb 50% Nerf / 40% physical & 40% spell damage buff'),
 (409, 0, '0.50', '0.50', '1.50', '1.50', 1, 'MC Healing 50% / Absorb 50% Nerf / 50% physical & 50% spell damage buff'),
